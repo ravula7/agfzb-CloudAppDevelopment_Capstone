83553fac908c32d9f67dcb9fd05fd7fe6c9de9f6
ravula7 <ravula7@ad.unc.edu>
2023-08-04 17:21:06 -0400

    done with week 1

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -10,6 +10,15 @@ urlpatterns = [
     # name the URL
 
     # path for about view
+    path(route='', view=views.get_dealerships, name='index'),
+    path('dealer/<int:id>/', views.get_dealer_details, name='dealer_details'),
+    path('dealer/<int:id>/review', views.add_review, name='add_review'),
+    path(route='about/', view=views.about, name='about'),
+    path(route='contact/', view=views.contact, name='contact'),
+    path('registration/', views.registration_request, name='registration'),
+    path('login/', views.login_request, name='login'),
+    path('logout/', views.logout_request, name='logout'),
+
 
     # path for contact us view
 
@@ -19,7 +28,6 @@ urlpatterns = [
 
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
 
     # path for dealer reviews view
 
