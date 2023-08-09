a02ebee0cb413bbbae39f999080f2863b6b975a5
ravula7 <ravula7@ad.unc.edu>
2023-08-09 15:22:09 -0400

    done w week 4

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -139,7 +139,7 @@ def analyze_review_sentiments(text):
     authenticator = IAMAuthenticator(api_key) 
     natural_language_understanding = NaturalLanguageUnderstandingV1(version='2021-08-01',authenticator=authenticator) 
     natural_language_understanding.set_service_url(url) 
-    response = natural_language_understanding.analyze( text=text ,features=Features(sentiment=SentimentOptions(targets=[text]))).get_result() 
+    response = natural_language_understanding.analyze( text=text, language='en',features=Features(sentiment=SentimentOptions(targets=[text]))).get_result() 
     label=json.dumps(response, indent=2) 
     label = response['sentiment']['document']['label'] 
     return(label) 
